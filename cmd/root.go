90c1c6d413536f91c74d446f8272f9b2944ce1b5
Francesco Romani <fromani@redhat.com>
2019-10-23 10:35:09 +0200

    better error reporting
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"github.com/fromanirh/pack8s/internal/pkg/podman"
 )
 
 // NewRootCommand returns entrypoint command to interact with all other commands
@@ -35,10 +37,9 @@ func NewRootCommand() *cobra.Command {
 
 }
 
-// Execute executes root command
 func Execute() {
 	if err := NewRootCommand().Execute(); err != nil {
-		fmt.Println(err)
+		fmt.Println(podman.SprintError("pack8s", err)) //XXX specific method
 		os.Exit(1)
 	}
 }
